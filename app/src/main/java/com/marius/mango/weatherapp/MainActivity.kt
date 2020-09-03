067655ec417b61b0374a0e6f29697447d7ebf186
DESKTOP-2GIO7N1\PC <mangovicius@gmail.com>
2020-09-03 13:26:37 +0300

    initial commit

---
+++ app/src/main/java/com/marius/mango/weatherapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.marius.mango.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marius.mango.weatherapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
